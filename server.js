78be0c020ba6e5d3b42cdabf0cb044a3936c470e
Oleksandr <savinoleksandr685@gmail.com>
2024-02-29 05:41:17 +0900

    modify server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const WebSocket = require("ws");
 
 const wss = new WebSocket.Server({ port: 3000 });
 const clients = [];
+const logos = {};
 
 function broadcast(message) {
   clients.forEach((client) => {
@@ -10,17 +11,73 @@ function broadcast(message) {
     }
   });
 }
+const s = "~m~";
+let rjscount = 0;
+
+function isValidJson(str) {
+  try {
+    JSON.parse(str);
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
+function _decode(e) {
+  const t = [];
+  let n, o;
+  do {
+    if (e.substring(0, 3) !== s)
+      return t;
+    n = "",
+      o = "";
+    const i = (e = e.substring(3)).length;
+    for (let t = 0; t < i; t++) {
+      if (o = Number(e.substring(t, t + 1)),
+        Number(e.substring(t, t + 1)) !== o) {
+        e = e.substring(n.length + s.length),
+          n = Number(n);
+        break
+      }
+      n += o
+    }
+    t.push(e.substring(0, n)),
+      e = e.substring(n)
+  } while ("" !== e);
+  return t
+}
 
 wss.on("connection", (ws) => {
   console.log("Client connected");
   clients.push(ws);
-
+  ws.send(JSON.stringify(logos));
   // Handle messages from the client
   ws.on("message", (message) => {
-    console.log(`Received message: ${typeof JSON.parse(message).data}`);
-
+    const data = JSON.parse(message).data;
+    rjscount++;
+    const t = _decode(data)
+    
     // Echo the message back to the client
-    broadcast(JSON.parse(message).data);
+    if (rjscount < 10) console.log(`Received message: ${t}`);
+    if(rjscount == 10) console.log(logos);
+
+
+
+    for (let e = 0; e < t.length; e++) {
+      
+      if (isValidJson(t[e])) {
+        const obj = JSON.parse(t[e]);
+        const v = obj.p[1].v;
+        
+        if (rjscount < 10 && v && v["base-currency-logoid"]) {
+          logos[obj.p[1].n] = {"base-currency-logoid": v["base-currency-logoid"], "currency-logoid": v["currency-logoid"]}          
+        }
+        if (rjscount < 10 && v && v["logoid"]) {
+          logos[obj.p[1].n] = {"logoid": v["logoid"]}          
+        }
+      }
+    }
+    broadcast(data);
   });
 
   // Handle disconnection
