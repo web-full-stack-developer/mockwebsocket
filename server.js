09af95c183f19483627121afe786470a9afb29df
Oleksandr <savinoleksandr685@gmail.com>
2024-03-04 08:26:06 +0900

    one server

--- server.js
+++ server.js
@@ -1,15 +1,33 @@
 const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 3001 });
 
+var clients = [];
+
+const targetWs = new WebSocket("wss://widgetdata.tradingview.com/socket.io/websocket", {
+  origin: 'https://www.tradingview.com'
+});
+
+function broadcast(message) {
+  clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(message);
+    }
+  });
+}
+
+targetWs.on('message', (message) => {
+  const binaryData = Buffer.from(message); // Example binary data
+  const text = binaryData.toString('utf8');
+  broadcast(text);
+});
+
+
 let connections = 0;
 wss.on('connection', (ws) => {
     connections ++;
     console.log("new add ", connections);
-    // Extract the target WebSocket URL from the request (e.g., query parameters)
-    const targetWs = new WebSocket("wss://widgetdata.tradingview.com/socket.io/websocket", {
-      origin: 'https://www.tradingview.com'
-    });
-
+    clients.push(ws);
+    
     // Proxy messages between the client and the target server
     ws.on('message', (message) => {
         try{
@@ -19,24 +37,10 @@ wss.on('connection', (ws) => {
         }
     });
 
-    targetWs.on('message', (message) => {
-      const binaryData = Buffer.from(message); // Example binary data
-      const text = binaryData.toString('utf8');
-      try {
-        ws.send(text);
-      } catch(e) {
-        console.error(e);
-      }
-      
-    });
-
     ws.on('close', () => {
-        targetWs.close();
+        clients.splice(clients.indexOf(ws), 1);
         connections --;
         console.log("close one ", connections);
     });
-
-    targetWs.on('close', () => {
-        ws.close();
-    });
+    
 });
