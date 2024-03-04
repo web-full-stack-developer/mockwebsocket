7169a80ad8dd917b8f0a4ed7d378cd8ade6f8347
danylodanylo12 <162181072+danylodanylo12@users.noreply.github.com>
2024-03-04 17:07:30 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-const WebSocket = require('ws'); 
+const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 3001 });
 
 var clients = [[], [], [], []];
+var initMsg = ["", "", "", ""];
 var targetWs = [];
 targetWs[0] = new WebSocket("wss://widgetdata.tradingview.com/socket.io/websocket?from=embed-widget%2Fmarket-overview%2F&date=2024_03_01-10_50&page-uri=www.tradingview.com%2Fwidget-wizard%2Fen%2Flight%2Fmarket-overview%2F", {
 	origin: 'https://www.tradingview.com'
@@ -27,21 +28,25 @@ function broadcast(id, message) {
 targetWs[0].on('message', (message) => {
 	const binaryData = Buffer.from(message); // Example binary data
 	const text = binaryData.toString('utf8');
+	if(!initMsg[0]) initMsg[0] = message;
 	broadcast(0, text);
 });
 targetWs[1].on('message', (message) => {
 	const binaryData = Buffer.from(message); // Example binary data
 	const text = binaryData.toString('utf8');
+	if(!initMsg[1]) initMsg[0] = message;
 	broadcast(1, text);
 });
 targetWs[2].on('message', (message) => {
 	const binaryData = Buffer.from(message); // Example binary data
 	const text = binaryData.toString('utf8');
+	if(!initMsg[2]) initMsg[0] = message;
 	broadcast(2, text);
 });
 targetWs[3].on('message', (message) => {
 	const binaryData = Buffer.from(message); // Example binary data
 	const text = binaryData.toString('utf8');
+	if(!initMsg[3]) initMsg[0] = message;
 	broadcast(3, text);
 });
 
@@ -66,7 +71,7 @@ wss.on('connection', (ws, req) => {
 	console.log(req.url);
 	connections++;
 	console.log("new add ", id, " ", connections);
-	ws.send('~m~346~m~{"session_id":"<0.4949.2378>_dal-charts-5-wgt-webchart-4@dal-compute-5","timestamp":1709512185,"timestampMs":1709512185140,"release":"registry.xtools.tv/tvbs_release/webchart:release_207-48","studies_metadata_hash":"0f1da59be5cb3ebc5873b524e394cf0792c5339d","auth_scheme_vsn":2,"protocol":"json","via":"209.58.153.118:443","javastudies":["3.64"]}');
+	ws.send(initMsg[id]);
 	clients[id].push(ws);
 
 	// Proxy messages between the client and the target server
